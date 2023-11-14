8d972eb0869844ae29eb9390555615542ab40f8f
wtfabduraim <rahmonkulovabduraim16@gmail.com>
2023-11-14 12:30:24 +0500

    3

---
+++ main/main.js
@@ -0,0 +1,46 @@
+// var UserName = "abu";
+// var UserName1 = "01";
+
+// console.log(UserName + UserName1);
+
+// var UserName = 10;
+// var UserName1 = 1;
+
+// console.log(UserName - UserName1);
+
+// var UserName = 3;
+// var UserName1 = 10;
+
+// console.log(UserName * UserName1);
+
+// var UserName = 25;
+// var UserName1 = 5;
+
+// console.log(UserName / UserName1);
+
+// var UserName = 11;
+// var UserName1 = 2;
+
+// console.log(UserName ** UserName1);
+
+// var UserName = 1.9;
+
+// console.log(UserName % UserName1);
+
+// var UserName1 = "11";
+
+// console.log(UserName1++);
+// console.log(UserName1);
+
+
+// var UserName1 = "11.1";
+
+// console.log(--UserName1);
+
+var abu = 11.1;
+
+// console.log(Math.floor(abu))
+
+var abu =Number(prompt(abu))
+
+alert (Math.floor(abu))
